db28ba46c4b03efd10a266c95608d671841aa04f
Melanie <mejr5b@gmail.com>
2019-12-05 17:31:08 -0600

    gradle

---
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -0,0 +1,46 @@
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@ResponseBody
+@RequestMapping("hello")
+public class HelloController {
+    //Handles request at path /hello
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello(){
+//        return "Hello, Spring!";
+//    }
+    //live /hello/goodbye
+    @GetMapping("goodbye")
+    public String goodbye() {
+        return "Goodbye, Spring!";
+    }
+//live /hello/hello
+    //Handles request of the form /hello?name=LaunchCode
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value="hello")
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
+    }
+
+    //Handles requests of the form /hello/LaunchCode
+    @GetMapping("{name}")
+    public String helloWithPathParam(@PathVariable String name) {
+        return "Hello, " + name + "!";
+
+    }
+
+    @GetMapping("form")
+    public String helloForm(){
+        return "<html>" +
+                "<body>" +
+                "<form action='hello' method='post'>" + //submit a request to /hello
+                "<input type = 'text' name='name'>" +
+                "<input type= 'submit' value='Greet me!'>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
+}
