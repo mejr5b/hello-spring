eaac1c8f9bded1dc74e7fb8c99034fc44cc70ea7
Melanie <mejr5b@gmail.com>
2019-12-05 18:52:32 -0600

    changed to a form

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -4,43 +4,37 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@ResponseBody
-@RequestMapping("hello")
+
 public class HelloController {
-    //Handles request at path /hello
-//    @GetMapping("hello")
-//    @ResponseBody
-//    public String hello(){
-//        return "Hello, Spring!";
-//    }
-    //live /hello/goodbye
-    @GetMapping("goodbye")
-    public String goodbye() {
-        return "Goodbye, Spring!";
-    }
-//live /hello/hello
-    //Handles request of the form /hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value="hello")
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    @RequestMapping(value = "/", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public String hello(@RequestParam(defaultValue = "World") String coder,
+                        @RequestParam(defaultValue = "en-us") String language){
+        if ("en-us".equals(language)) {
+            return "<h1>Hello " + coder.toUpperCase() + "!</h1>";
+        } else if ("en-fr".equals(language)) {
+            return "<h1>Bonjour " + coder.toUpperCase() + "!</h1>";
+        } else if ("en-sp".equals(language)){
+            return "<h1>Hola " + coder.toUpperCase() + "!</h1>";
+        } else {
+            return "<h1>Hello " + coder.toUpperCase() + "!</h1>";
+        }
     }
 
-    //Handles requests of the form /hello/LaunchCode
-    @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
-
-    }
-
-    @GetMapping("form")
+    @GetMapping("/helloform")
+    @ResponseBody
     public String helloForm(){
         return "<html>" +
-                "<body>" +
-                "<form action='hello' method='post'>" + //submit a request to /hello
-                "<input type = 'text' name='name'>" +
-                "<input type= 'submit' value='Greet me!'>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+                "<body><h3>" +
+                "<form action= '/' method='post'>" + //submit a request to /hello
+                "<input type = 'text' name='coder'/>" +
+             "<select name = 'language'>\n" +
+                " <option value=\"en-fr\">French</option>\n" +
+                " <option value=\"en-us\">English</option>\n" +
+                " <option value=\"en-sp\">Spanish</option>\n" +
+                "</select>" + "<input type = 'submit' value = 'Greet Me!' />" +
+                "</body></h3>" +
+                        "</form>" +
+                        "</html>";
     }
 }
